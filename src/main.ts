349e66e1137aa6c1546a22f98137e55e31077bc7
Patricio Pilco <ppilco@galavail.com>
2024-05-05 11:33:21 -0500

    chore: Add global validation pipe to microservice

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { envs } from './config/envs';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule,{
@@ -10,6 +11,13 @@ async function bootstrap() {
       servers: envs.natsServers
     }
   });
+
+  app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+      }),
+  );
   await app.listen();
 }
 bootstrap();
